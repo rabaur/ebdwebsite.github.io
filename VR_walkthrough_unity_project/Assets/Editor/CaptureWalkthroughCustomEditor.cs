b8ee52e9e79a49b0bac887dcac569f1755b9d993
awelski <rabaur@student.ethz.ch>
2022-03-11 15:29:07 +0100

    Removed folders which hold data.

---
+++ VR_walkthrough_unity_project/Assets/Editor/CaptureWalkthroughCustomEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CaptureWalkthrough))]
+public class CaptureWalkthroughCustomEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Press me!"))
+        {
+            Debug.Log("UWU. I was pressed.");
+        }
+        EditorUtility.OpenFilePanel("Choose a folder to save data to");
+    }
+}
